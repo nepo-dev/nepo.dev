48de62546f9bc1b0d52ee18fac2108d2cb9fa14b
edearth <nepormoseno1992@gmail.com>
2020-08-29 05:04:34 +0200

    Built initial post feed generator

--- build-site.sh
+++ build-site.sh
@@ -1,5 +1,50 @@
 #!/bin/sh
 
-# do this for each article
-pandoc -s "$1" --template article-template.html -o "converted.html"
-pandoc posts/universal-document-language.md --template feed-item-template.html >> test.html #this you add to the index page
+ARTICLE_TEMPLATE="templates/article.html"
+PREVIEW_TEMPLATE="templates/preview.html"
+PREVIEW_TEMPLATE_REPLACE_KEYWORD="<!--URL-->"
+INDEX_TEMPLATE="templates/index.html"
+INDEX_CONTENT_TEMP_FILE="temp.html"
+INDEX_TEMPLATE_REPLACE_KEYWORD="<!--CONTENT-->"
+INDEX_FILE="index.html"
+POST_FOLDER="posts"
+
+
+get_posts_with_date() {
+  for post in $(ls $POST_FOLDER | grep '.md'); do
+    date=$(grep -Po '(?<=date:)\s?\d{4}-\d{2}-\d{2}' $POST_FOLDER/$post | sed -e 's/^[ \t]*//')
+    echo "$date $post"
+  done
+}
+
+get_ordered_post_list() {
+  get_posts_with_date | sort -r | awk '{print $2}'
+}
+
+generate_index_preview() {
+  pandoc "$POST_FOLDER/$1" --template "$PREVIEW_TEMPLATE" >> "$INDEX_CONTENT_TEMP_FILE"
+  post_url="$POST_FOLDER/${1%.*}.html"
+  #sed -e "s#<\!--URL-->#$post_url#" temp.html > temp.html
+  sed -ie "s#<!--URL-->#$post_url#" "$INDEX_CONTENT_TEMP_FILE"
+}
+
+generate_article() {
+  pandoc -s "$POST_FOLDER/$1" --template "$ARTICLE_TEMPLATE" -o "$POST_FOLDER/${1%.*}.html"
+}
+
+generate_index() {
+  cp "$INDEX_TEMPLATE" "$INDEX_FILE"
+  sed -i "/^$INDEX_TEMPLATE_REPLACE_KEYWORD/ r $INDEX_CONTENT_TEMP_FILE" "$INDEX_FILE"
+}
+
+for post in $(get_ordered_post_list); do
+  echo "Generating ${post%.*}.html"
+  generate_article "$post"
+  generate_index_preview "$post"
+done
+
+echo "Generating index.html"
+generate_index 
+rm "$INDEX_CONTENT_TEMP_FILE"
+echo "Build finished ✅"
+
